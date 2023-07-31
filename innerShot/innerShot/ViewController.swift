6461e8bf461aff3506724f4dcc5698b9333a6b72
Meet Shingala <shingalameet4877@gmail.com>
2023-08-01 00:47:12 +0530

    store screenShot to photos library

--- innerShot/innerShot/ViewController.swift
+++ innerShot/innerShot/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import Photos
+
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: BTNEvents
+    @IBAction func btnTakeScreenshot(_ sender: Any) {
+        takeScreenshotAndSave()
+    }
+    
+    //MARK: Functions
+    func takeScreenshotAndSave() {
+        
+        guard let view = UIApplication.shared.windows.first?.rootViewController?.view else {
+            return
+        }
+        
+        // Create an image renderer
+        let renderer = UIGraphicsImageRenderer(size: view.bounds.size)
+        
+        // Render the view into an image
+        let screenshotImage = renderer.image { context in
+            view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        }
+        
+        // Save the screenshot to the Photos library
+        UIImageWriteToSavedPhotosAlbum(screenshotImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
 }
 
+extension ViewController {
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeMutableRawPointer?) {
+        if let error = error {
+            // Handle error saving the image
+            showToast(message: "Error saving image: \(error.localizedDescription)")
+        } else {
+            // Image saved successfully
+            showToast(message: "ScreenShot Taken")
+        }
+    }
+    
+    func showToast(message: String) {
+        let toast = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        UIApplication.shared.keyWindow?.rootViewController?.present(toast, animated: true, completion: {
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+                toast.dismiss(animated: true)
+            }
+        })
+    }
+}
